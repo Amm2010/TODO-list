981e4c3db4aeafe8a7547541acff2b0c8ae587a4
Amm2010 <ammar2010428@gmail.com>
2025-01-24 16:55:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var notesArray = [];
+const notesInput = document.getElementById("notesInput");
+const itemCount = document.getElementById("itemCount");
+const listDiv = document.getElementById("listDiv");
+
+function addNote() {
+    const newNote = notesInput.value;
+    if (newNote == "") {
+        alert("enter your note")
+    } else {
+        notesArray.push(newNote)
+        notesInput.value = "";
+        itemCount.innerText = notesArray.length;
+        console.log(notesArray);     
+    }    
+}
+
+function showList(){
+    for (let index = 0; index < notesArray.length; index++) {
+        const element = notesArray[index];
+        
+        const newLi = document.createElement("li");
+
+        newLi.innerText = element;
+
+        listDiv.appendChild(newLi)
+    }    
+}
